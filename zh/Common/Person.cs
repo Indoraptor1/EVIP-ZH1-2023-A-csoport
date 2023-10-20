4e5b8c7949fa8e69f1fc6a1e153975d2671f73dd
Zsombor <zsombor@sky-frames.com>
2023-10-21 00:09:46 +0200

    Person és TrivialPersonSource

---
+++ zh/Common/Person.cs
@@ -0,0 +1,19 @@
+﻿namespace Common;
+
+public class Person
+{
+    public string name { get; set; }
+    public int age { get; set; }
+    public int height { get; set; }
+
+    public Person(string name, int age, int height){
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    public static Person GetMe()
+    {
+        return new Person("Zsombor", 20, 180);
+    }
+}
