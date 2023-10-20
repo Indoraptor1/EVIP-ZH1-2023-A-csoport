35c539d4dfc07c4583a127e436873b425a5152e7
Zsombor <zsombor@sky-frames.com>
2023-10-21 00:29:43 +0200

    vége

--- zh/ConsoleApplication/Program.cs
+++ zh/ConsoleApplication/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Common;
+
+TrivialPersonSource tsource = new(3);
+AdvancedPersonSource asource = new();
+asource.AddPerson("John", 12, 123);
+asource.AddPerson("Big Joe", 112, 80);
+asource.AddPerson("ASD", 52, 55);
+asource.AddPerson("ASD2", 52, 55);
+foreach (var person in tsource.GetPersons())
+{
+    Console.WriteLine(person.ToString());
+}
+
+foreach (var person in asource.GetPersons())
+{
+    Console.WriteLine(person.ToString());
+}
