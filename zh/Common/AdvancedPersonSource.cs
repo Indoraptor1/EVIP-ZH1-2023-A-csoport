932803f704a5ec55c3228aaa78a37c52e2f9e82f
Zsombor <zsombor@sky-frames.com>
2023-10-21 00:19:29 +0200

    aw persons

---
+++ zh/Common/AdvancedPersonSource.cs
@@ -0,0 +1,20 @@
+﻿namespace Common;
+
+public class AdvancedPersonSource : IPersonSource
+{
+    public List<Person> Persons { get; set; }
+    public IEnumerable<Person> GetPersons()
+    {
+        return Persons;
+    }
+
+    public void AddPerson(string name, int age, int height)
+    {
+        Persons.Add(new Person(name, age, height));
+    }
+
+    public AdvancedPersonSource()
+    {
+        Persons = new List<Person>();
+    }
+}
